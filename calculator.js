b14ae8120ab2c0fa08d1d3f5b9a0f7c857da8c6f
Tyler Bamberg <7506119+tbamberg@users.noreply.github.com>
2023-11-09 16:43:10 -0800

    Add basic calc functions and an operator handler

--- calculator.js
+++ calculator.js
@@ -1,1 +1,23 @@
-console.log("test");
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, firstNumber, secondNumber) {
+    return operator(firstNumber, secondNumber);
+}
