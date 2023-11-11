2545f51e9b2accc813029865ebaf86490a0d62f2
Tyler Bamberg <7506119+tbamberg@users.noreply.github.com>
2023-11-11 01:25:41 -0800

    Add functions to handle button clicks and display

--- calculator.js
+++ calculator.js
@@ -1,23 +1,55 @@
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = '';
+let display = document.querySelector('#display');
 
-function add(a, b) {
-    return a + b;
-}
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
 
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function operate(operator, firstNumber, secondNumber) {
+function operate(firstNumber, operator, secondNumber) {
     return operator(firstNumber, secondNumber);
 }
+
+function setDisplay(num) {
+    display.textContent = num;
+}
+
+function turnOn() {
+    if(display.textContent == '') {
+        setDisplay(0);
+    }
+}
+
+function clear() {
+    firstNumber = 0;
+    operator = 0;
+    secondNumber = 0;
+    setDisplay(0);
+}
+
+function setInput(button) {
+    //check for non-digit buttons
+    if (button.id == 'on') {
+        turnOn();
+    } else if (button.id == 'clear') {
+        clear();
+    } else {
+        if (display.textContent == '0' || display.textContent == '') {
+            //remove leading 0
+            firstNumber = button.id;
+        } else {
+            //update firstNumber
+            firstNumber = firstNumber + button.id;
+        }
+        setDisplay(firstNumber);
+    }
+}
+
+let buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        setInput(button);
+    });
+});
